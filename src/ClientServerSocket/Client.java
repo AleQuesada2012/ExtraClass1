dda9982a453de15e19cd0ed2906d10bcd07aa5e8
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-03-02 21:58:45 -0600

    Added multi-line header in Client.java, with two imports for packages to be used within the code

--- src/ClientServerSocket/Client.java
+++ src/ClientServerSocket/Client.java
@@ -1,4 +1,17 @@
+/*
+*********************************
+Costa Rican Insitute of Technology
+School of Computer Engineering
+Algorithms and Data Structures I
+Student: Alejandro Jose Quesada Calderon
+Teacher: Diego Noguera Mena
+Assignment: ExtraClass1
+*********************************
+ */
 package ClientServerSocket;
+import java.net.*; //imported the package of java Networking API
+import java.io.*;  //imported package for input/output
 
 public class Client {
+
 }
