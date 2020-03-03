b6d5b50fe2f6c032867336b0288739a8f6cf1612
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-03-03 04:13:15 -0600

    Created a new class to start implementing Threads to the code, this class is still not properly developed and might be refactored over time to integrate threadding into the project.

---
+++ src/ClientServerSocket/NewClientThread.java
@@ -0,0 +1,9 @@
+package ClientServerSocket;
+import java.net.*;
+import java.io.*;
+
+public class NewClientThread extends Thread {
+    //here we see the first explicit example of inheritance, as our class "NewClientThread" extends, and therefore
+    //inherits the methods, objects and fields/attributes from its superclass Thread.
+
+}
