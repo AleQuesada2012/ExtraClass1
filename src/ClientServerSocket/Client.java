e8f5d1e47c710d4b5e06b63a14416321fedd3c0d
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-03-03 00:27:12 -0600

    Temporary code for a single client connection based on the guidelines of https://www.geeksforgeeks.org/socket-programming-in-java/ article and tutorial; last comment added in line 72 of this commit states what is to be done in following modifications to this project.

--- src/ClientServerSocket/Client.java
+++ src/ClientServerSocket/Client.java
@@ -21,10 +21,10 @@ public class Client {
     //the last three attributes have not been assigned with any value so far
 
     //constructor with the arguments containing IP address and port for communication establishment
-    public Client(String IPadd, int PortNumber){
+    public Client(String IPadd, int PortNumber) {
 
         //try-catch used to attempt a connection without stopping the code in runtime if connection is not established.
-        try{
+        try {
             socket1 = new Socket(IPadd, PortNumber);
             System.out.println("Client connected");
 
@@ -35,14 +35,38 @@ public class Client {
             //method used is different from input due to the direction information is headed
             output = new DataOutputStream(socket1.getOutputStream());
 
-        }catch(UnknownHostException Unknown){
+        } catch (UnknownHostException Unknown) {
             //this code segment runs in case if the connection does not succeed, but it stops the code from stopping
             System.out.println(Unknown);
-
-        }catch(IOException Ioexcpt){
+            //different types of Exception subclasses used to specify the detection of a problem and excecute another section of code instead of a general Exception.
+        } catch (IOException Ioexcpt) {
             System.out.println(Ioexcpt);
         }
+        //String used to store the messages read from the user
+        String Message = "";
+
+        //while condition to mantain connection until predetermined keyword is received by the server
+        while ((!Message.equals("Over") || (!Message.equals("Out")))) {
+            try {
+                Message = input.readLine();
+                output.writeUTF(Message); //this method is used as the codification used by both parties follows the UTF standard.
+            } catch (IOException Ioexcpt) {
+                System.out.println(Ioexcpt);
+            }
+
+        }
+        try {
+            input.close();
+            output.close();
+            socket1.close();
+
+        } catch (IOException Ioexcpt) {
+            System.out.println(Ioexcpt);
+        }
+    }
+    public static void main(String[] args){
+        Client Client1 = new Client("127.0.0.1", 128); //IPadd set to a value predetermined by the teacher in task specifications.
 
     }
-
 }
+//finished programming for one client, preparing Server code for further console test before implementing Threads and JavaFX GUI.
