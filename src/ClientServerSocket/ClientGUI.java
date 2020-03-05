2e6b1a57f1857317c90ecb503d6223ca7b168120
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-03-04 23:37:52 -0600

    Final commit, failed to implement GUI with JavaFX to the solution, but the chat is fully operational in the console.

--- src/ClientServerSocket/ClientGUI.java
+++ src/ClientServerSocket/ClientGUI.java
@@ -10,9 +10,12 @@
  * //the next imports are suggested by youtube user "thenewboston" from tutorial:
  * //https://www.youtube.com/watch?v=FLkOX4Eez6o
  */
+
 package ClientServerSocket;
 
 import javafx.application.Application;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,11 +25,15 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 import javax.swing.*;
+import javax.xml.soap.Text;
 
 //inheritance
 public class ClientGUI extends Application implements EventHandler<ActionEvent>{ //from javaFX
 
     Button sendButton;
+    TextArea textSquare;
+    TextField textEntry;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -35,12 +42,28 @@ public class ClientGUI extends Application implements EventHandler<ActionEvent>{
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Client");
         sendButton = new Button("Send");
+        sendButton.setOnAction(this);
+        sendButton.setLayoutX(500);
+        sendButton.setLayoutY(500);
+
+        textSquare = new TextArea("Welcome to the Chat!");
+
+
+        textEntry = new TextField();
+        textEntry.setLayoutX(0);
+        textEntry.setLayoutY(355);
+
         StackPane layout = new StackPane();
+
+        Scene scene = new Scene(layout,550,550);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        layout.getChildren().add(textSquare);
+        layout.getChildren().add(textEntry);
         layout.getChildren().add(sendButton);
 
-        Scene scene = new Scene(layout,500,500);
-        primaryStage.getScene(scene);
-        primaryStage.show();
 
     }
 
@@ -50,8 +73,12 @@ public class ClientGUI extends Application implements EventHandler<ActionEvent>{
      *
      * @param event the event which occurred
      */
+
     @Override
     public void handle(ActionEvent event) {
+        if( event.getSource() == sendButton){
+            //add code to send the message to the other clients
 
+        }
     }
 }
